fa811926fbf74606dc1d970ba1a0feaab93b9e16
Евгений Боярчук <92888200+InsanivimusOmnes@users.noreply.github.com>
2021-10-28 18:13:45 +0300

    Add files via upload

---
+++ HomeDef.py
@@ -0,0 +1,15 @@
+def colculator(a, b, c):
+    if c == '+':
+        res = a + b
+    if c == '-':
+        res = a - b
+    if c == '*':
+        res = a * b
+    if c == '/':
+        res = a / b
+    return res
+
+print(colculator(33, 11, '/'))
+
+
+
