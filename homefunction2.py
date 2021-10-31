8633bec654707713bec10126eec7534724e4684e
Евгений Боярчук <92888200+InsanivimusOmnes@users.noreply.github.com>
2021-10-31 23:22:27 +0300

    Add files via upload

--- homefunction2.py
+++ homefunction2.py
@@ -1,28 +1,28 @@
-# def function(x):
-#     if x < -2:
-#         res = 1 - (x + 2)**2
-#     elif x in range(-2, 3):
-#         res = - x / 2
-#     elif x > 2:
-#         res = (x - 2)**2 + 1
-#     return res
-#
-#
-# print(function(2))
+def function(x):
+    if x < -2:
+        res = 1 - (x + 2)**2
+    elif x in range(-2, 3):
+        res = - x / 2
+    elif x > 2:
+        res = (x - 2)**2 + 1
+    return res
 
 
-# def append_list(*args):
-#     ls = []
-#     for i in args:
-#         rev = 0
-#         while i:
-#             last = i % 10
-#             rev += last
-#             i = i // 10
-#         ls.append(rev)
-#     return sorted(ls)
-#
-# print(append_list(323, 21))
+print(function(2))
+
+
+def append_list(*args):
+    ls = []
+    for i in args:
+        rev = 0
+        while i:
+            last = i % 10
+            rev += last
+            i = i // 10
+        ls.append(rev)
+    return sorted(ls)
+
+print(append_list(323, 21))
 
 
 def number_list(*args):
